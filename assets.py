911c3ddb299c8d306a6db00e24f80327d00b3f5a
Circuitbreaker08 <53588678+Circuitbreaker08@users.noreply.github.com>
2024-02-13 15:30:35 -0500

    started logics

--- assets.py
+++ assets.py
@@ -3,3 +3,10 @@ Loads everything into memory at the start of runtime
 """
 
 import os
+
+sprites = {}
+root = os.getcwd()
+
+os.cwd(os.path.join(root, "assets/sprites"))
+for dir in os.listdir():
+    pass
