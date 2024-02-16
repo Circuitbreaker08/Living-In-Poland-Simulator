e36a1dd9f5496f59cb3a2f44218f765939bb4068
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-02-15 23:18:20 -0500

    Added movemnt, bashed head in wall for collision, had moral debate on Vosh, then made Bocchi the Rock stare at you friendily

--- assets.py
+++ assets.py
@@ -15,8 +15,12 @@ root = os.getcwd()
 for directory in os.listdir():
     sprites.update({directory: {}})
     os.chdir(os.path.join(root, directory))
+    if directory == "portraits":
+        size = (256, 256)
+    else:
+        size = (64, 64)
     for file in os.listdir():
-        sprites[directory].update({file: pygame.image.load(file).convert_alpha()})
+        sprites[directory].update({file: pygame.transform.scale(pygame.image.load(file).convert_alpha(), size)})
 
 os.chdir(os.path.join(os.path.split(__file__)[0], "assets/maps"))
 root = os.getcwd()
@@ -24,5 +28,3 @@ root = os.getcwd()
 for file in os.listdir():
     with open(file) as f:
         maps.update({file: json.loads(f.read())})
-
-print(sprites)
--- main.py
+++ main.py
@@ -8,6 +8,28 @@ pygame.display.set_caption("Living In Poland Simulator")
 from assets import sprites, maps
 
 loaded_map = "test_room_1.json"
+position = [128, 128]
+
+def move(x, y):
+    global position
+    """
+    for wall in maps[loaded_map]["walls"]:
+        if pygame.Rect((position[0], position[1]), (position[0] + 64, position[1] + 64)).colliderect(pygame.Rect()):
+            break
+    else:
+        position[0] += x
+
+    for wall in maps[loaded_map]["walls"]:
+        if False:
+            break
+    else:
+        position[1] += y
+    """
+        
+def chat(message, portrait):
+    portrait = sprites["portraits"][portrait]
+    screen.blit(portrait, (200, 700))
+    screen.blit(pygame.font.Font(None, 48).render(message, False, (255, 255, 255)), (500, 800))
 
 running = True
 while running:
@@ -15,10 +37,19 @@ while running:
         if event == pygame.QUIT:
             running = False
 
+    screen.fill((0, 0, 0))
+
+    move(
+        3 * (pygame.key.get_pressed()[pygame.K_d] - pygame.key.get_pressed()[pygame.K_a]),
+        3 * (pygame.key.get_pressed()[pygame.K_s] - pygame.key.get_pressed()[pygame.K_w])
+    )
+
     for tile in maps[loaded_map]["floors"] + maps[loaded_map]["walls"]:
-        screen.blit(sprites["mapping"][tile["sprite"]], (tile["x"] * 32 + 960, tile["y"] * 32 + 540))
+        screen.blit(sprites["mapping"][tile["sprite"]], (tile["x"] * 64 + 960 - position[0], tile["y"] * 64 + 540 - position[1]))
 
     screen.blit(sprites["characters"]["debug.png"], (960, 540))
+
+    chat("Hi, Polski-san!", "bocchi_1000_yard_stare.png")
 
     pygame.display.flip()
     clock.tick(60)
