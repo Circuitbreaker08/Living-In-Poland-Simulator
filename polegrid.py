c95ca93c49c433be92b1adc616fad2276d1d2294
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-02-21 22:52:58 -0500

    Refined editor tile selection; added blank map template

--- polegrid.py
+++ polegrid.py
@@ -44,9 +44,10 @@ while running:
     for tile in data["floors"] + data["walls"]:
         screen.blit(sprites["mapping"][tile["sprite"]], (tile["x"] * 64 + 960 - position[0], tile["y"] * 64 + 540 - position[1]))
 
-    mouse_world = [mouse[0] + position[0], mouse[1] + position[1]]
+    mouse_world = [mouse[0] - 960 + position[0], mouse[1] - 540 + position[1]]
     tile = [math.floor(mouse_world[0] / 64), math.floor(mouse_world[1] / 64)]
-    pygame.draw.rect(screen, (0, 0, 200, 50), pygame.rect.Rect((0, 0), (0, 0)))
+    pygame.draw.rect(screen, (0, 200, 0, 50), pygame.rect.Rect((tile[0] * 64 + 960 - position[0], tile[1] * 64 + 540 - position[1]), (64, 64)))
+    screen.blit(pygame.font.Font(None, 48).render(str(tuple(tile)), False, (255, 255, 255)), (0, 70))
 
     pygame.display.flip()
     clock.tick(60)
