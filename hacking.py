2eed2a6483a947b2a26f93ed6cbd6bef290088b4
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-02-27 17:16:56 -0500

    Hacking end logic

--- hacking.py
+++ hacking.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import sys
 
 screen: pygame.Surface = getattr(sys.modules["__main__"], "screen")
@@ -8,18 +9,34 @@ def init():
     target = 45
     target_range = 50
     actual = 50
-    progress = 10
+    progress = 20
     goal = 100
+
+def victory():
+    finish()
+
+def defeat():
+    finish()
 
 def finish():
     setattr(sys.modules["__main__"], "is_hacking", False)
 
 def hack():
-    global actual
+    global actual, progress
     if pygame.mouse.get_pressed()[0]:
         actual = min(actual + 3, 959)
     else:
         actual = max(actual - 3, 1)
+
+    if actual > target and actual < target + target_range:
+        progress += 1.5
+    else:
+        progress -= 0.5
+
+    if progress >= 100:
+        victory()
+    elif progress <= 0:
+        defeat()
 
     pygame.draw.rect(screen, (0, 0, 0), pygame.rect.Rect((250, 250), (1420, 580)))
     pygame.draw.rect(screen, (255, 255, 255), pygame.rect.Rect((250, 250), (1420, 580)), width=5)
@@ -34,7 +51,7 @@ def hack():
     pygame.draw.rect(screen, (25, 25, 170), pygame.rect.Rect((500 + target, 451), (target_range, 28)))
     pygame.draw.rect(screen, (230, 0, 0), pygame.rect.Rect((500 + actual, 451), (10, 28)))
 
-    img = pygame.font.Font(None, 48).render(f"{int(progress/goal)}%", False, (255, 255, 255))
+    img = pygame.font.Font(None, 48).render(f"{int(progress/goal * 100)}%", False, (255, 255, 255))
     screen.blit(img, (960 - img.get_width()/2, 600))
     pygame.draw.rect(screen, (255, 255, 255), pygame.rect.Rect((500, 650), (970, 30)), width=1)
     pygame.draw.rect(screen, (0, 255, 0), pygame.rect.Rect((500, 650), ((progress/goal) * 970, 30)))
